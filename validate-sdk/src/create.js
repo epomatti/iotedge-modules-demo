726c436ceef28a78a1dfb6edc23c217eecdb3f2f
Evandro Pomatti <evandro.pomatti@gmail.com>
2020-03-22 20:38:15 -0300

    create

--- validate-sdk/src/create.js
+++ validate-sdk/src/create.js
@@ -4,8 +4,10 @@ var iothub = require('azure-iothub');
 var connectionString = process.env.IOTHUB_CONNECTION_STRING;
 var registry = iothub.Registry.fromConnectionString(connectionString);
 
+deviceId = 'test001'
+
 device = {
-    deviceId: 'test001',
+    deviceId: deviceId,
     status: 'enabled',
     capabilities: {
         iotEdge: true
@@ -13,7 +15,7 @@ device = {
 }
 
 async function listDevices() {
-    await registry.delete('test001').then(response => {
+    await registry.delete(deviceId).then(response => {
         console.log(response);
     });
     await registry.create(device).then(response => {
